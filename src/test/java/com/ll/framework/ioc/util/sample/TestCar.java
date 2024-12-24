acc22da38ed6929b960d762afd6fd4b9dbfa278f
jhs512 <jangka512@gmail.com>
2024-12-24 17:17:21 +0900

    t2 r

--- src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
+++ src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
@@ -1,8 +1,10 @@
 package com.ll.framework.ioc.util.sample;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class TestCar {
     private String name;
     private int number;
