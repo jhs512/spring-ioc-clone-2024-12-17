66844d976d0198d0499ab1d47a8add1c38d737f3
jhs512 <jangka512@gmail.com>
2024-12-24 17:35:47 +0900

    t6 g

--- src/main/java/com/ll/framework/ioc/util/ClsUtil.java
+++ src/main/java/com/ll/framework/ioc/util/ClsUtil.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
 
 public class ClsUtil {
     @SneakyThrows
@@ -34,5 +35,17 @@ public class ClsUtil {
         Constructor<T> constructor = getConstructor(cls, args);
 
         return constructor.getParameters();
+    }
+
+    public static String[] getParameterNames(String clsPath, Object[] args) {
+        return getParameterNames(loadClass(clsPath), args);
+    }
+
+    public static <T> String[] getParameterNames(Class<T> cls, Object[] args) {
+        return Arrays.stream(
+                        getParameters(cls, args)
+                )
+                .map(Parameter::getName)
+                .toArray(String[]::new);
     }
 }
