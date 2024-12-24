947ae3ae1726b26cde880e193b6e5ff450058ca3
jhs512 <jangka512@gmail.com>
2024-12-24 17:21:27 +0900

    t2 b

--- src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
+++ src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
@@ -16,9 +16,20 @@ public class ClsUtilTest {
     }
 
     @Test
-    @DisplayName("ClsUtil.construct")
+    @DisplayName("ClsUtil.construct with clsPath")
     void t2() {
         TestCar testCar = ClsUtil.construct("com.ll.framework.ioc.util.sample.TestCar", new Object[]{"BMW", 1234});
+        // 위 코드는 아래와 같은 의미이다.
+        // new TestCar("BMW", 1234);
+
+        assertThat(testCar.getName()).isEqualTo("BMW");
+        assertThat(testCar.getNumber()).isEqualTo(1234);
+    }
+
+    @Test
+    @DisplayName("ClsUtil.construct with cls")
+    void t3() {
+        TestCar testCar = ClsUtil.construct(TestCar.class, new Object[]{"BMW", 1234});
         // 위 코드는 아래와 같은 의미이다.
         // new TestCar("BMW", 1234);
 
