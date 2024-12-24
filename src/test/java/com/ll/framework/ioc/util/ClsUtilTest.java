02925c13616ff0492cc1edb0861200a5a43616e5
jhs512 <jangka512@gmail.com>
2024-12-24 17:09:58 +0900

    t1

---
+++ src/test/java/com/ll/framework/ioc/util/ClsUtilTest.java
@@ -0,0 +1,17 @@
+package com.ll.framework.ioc.util;
+
+import com.ll.framework.ioc.util.sample.TestCar;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClsUtilTest {
+    @Test
+    @DisplayName("ClsUtil.loadClass")
+    void t1() {
+        Class<TestCar> cls = ClsUtil.loadClass("com.ll.framework.ioc.util.sample.TestCar");
+
+        assertThat(cls).isEqualTo(TestCar.class);
+    }
+}
---
+++ src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
@@ -0,0 +1,9 @@
+package com.ll.framework.ioc.util.sample;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TestCar {
+    private String name;
+    private int number;
+}
